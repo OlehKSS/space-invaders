13540b32eab8f4e81cc2c1eec7bb9d6553d86daf
Oleh Kozynets <oleh.kozynets@gmail.com>
2025-03-30 19:39:21 +0200

    Add animated aliens and a player.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <cstdint>
+#include <memory>
 #include <print>
+#include <vector>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -7,14 +9,47 @@ struct Buffer
 {
     size_t width;
     size_t height;
-    uint32_t* data;
+    std::vector<uint32_t> data;
 };
 
 struct Sprite
 {
     size_t width;
     size_t height;
-    uint8_t* data;
+    std::vector<uint8_t> data;
+};
+
+// A position (x, y) given in pixels from the bottom left corner of the window
+struct Alien
+{
+    size_t x;
+    size_t y;
+    uint8_t type;
+};
+
+struct Player
+{
+    size_t x;
+    size_t y;
+    size_t life;
+};
+
+struct Game
+{
+    size_t width;
+    size_t height;
+    size_t num_aliens;
+    std::vector<Alien> aliens;
+    Player player;
+};
+
+struct SpriteAnimation
+{
+    bool loop;
+    size_t num_frames;
+    size_t frame_duration;
+    size_t time;
+    std::vector<Sprite> frames;
 };
 
 constexpr uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b)
@@ -125,12 +160,15 @@ int main(int argc, char** argv)
         return -1;
     }
 
+    // Turn on V-sync, an option wherein video card updates are synchronized with the monitor refresh rate, e.g., 60 Hz
+    glfwSwapInterval(1);
+
     glClearColor(1.0, 0.0, 0.0, 1.0);
 
     Buffer buffer;
     buffer.width = buffer_width;
     buffer.height = buffer_height;
-    buffer.data = new uint32_t[buffer.width * buffer.height];
+    buffer.data = std::vector<uint32_t>(buffer.width * buffer.height);
     buffer_clear(&buffer, 0);
 
     GLuint buffer_texture;
@@ -140,7 +178,7 @@ int main(int argc, char** argv)
     glTexImage2D(
         GL_TEXTURE_2D, 0, GL_RGB8,
         buffer.width, buffer.height, 0,
-        GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, buffer.data
+        GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, buffer.data.data()
     );
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -210,7 +248,6 @@ void main(void) {
         std::println("Error while validating shader.");
         glfwTerminate();
         glDeleteVertexArrays(1, &fullscreen_triangle_vao);
-        delete[] buffer.data;
         return -1;
     }
 
@@ -225,10 +262,10 @@ void main(void) {
 
     glBindVertexArray(fullscreen_triangle_vao);
 
-    Sprite alien_sprite;
-    alien_sprite.width = 11;
-    alien_sprite.height = 8;
-    alien_sprite.data = new uint8_t[11 * 8]
+    Sprite alien_sprite0;
+    alien_sprite0.width = 11;
+    alien_sprite0.height = 8;
+    alien_sprite0.data = std::vector<uint8_t>
     {
         0,0,1,0,0,0,0,0,1,0,0, // ..@.....@..
         0,0,0,1,0,0,0,1,0,0,0, // ...@...@...
@@ -240,28 +277,122 @@ void main(void) {
         0,0,0,1,1,0,1,1,0,0,0  // ...@@.@@...
     };
 
+    Sprite alien_sprite1;
+    alien_sprite1.width = 11;
+    alien_sprite1.height = 8;
+    alien_sprite1.data = std::vector<uint8_t>
+    {
+        0,0,1,0,0,0,0,0,1,0,0, // ..@.....@..
+        1,0,0,1,0,0,0,1,0,0,1, // @..@...@..@
+        1,0,1,1,1,1,1,1,1,0,1, // @.@@@@@@@.@
+        1,1,1,0,1,1,1,0,1,1,1, // @@@.@@@.@@@
+        1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+        0,1,1,1,1,1,1,1,1,1,0, // .@@@@@@@@@.
+        0,0,1,0,0,0,0,0,1,0,0, // ..@.....@..
+        0,1,0,0,0,0,0,0,0,1,0  // .@.......@.
+    };
+
+    auto alien_animation = std::make_unique<SpriteAnimation>();
+    alien_animation->loop = true;
+    alien_animation->num_frames = 2;
+    alien_animation->frame_duration = 10;
+    alien_animation->time = 0;
+    alien_animation->frames =std::vector<Sprite>(2);
+    alien_animation->frames[0] = alien_sprite0;
+    alien_animation->frames[1] = alien_sprite1;
+
+
+    Sprite player_sprite;
+    player_sprite.width = 11;
+    player_sprite.height = 7;
+    player_sprite.data = std::vector<uint8_t>
+    {
+        0,0,0,0,0,1,0,0,0,0,0, // .....@.....
+        0,0,0,0,1,1,1,0,0,0,0, // ....@@@....
+        0,0,0,0,1,1,1,0,0,0,0, // ....@@@....
+        0,1,1,1,1,1,1,1,1,1,0, // .@@@@@@@@@.
+        1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+        1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+        1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
+    };
+
+    Game game;
+    game.width = buffer_width;
+    game.height = buffer_height;
+    game.num_aliens = 55;
+    game.aliens = std::vector<Alien>(game.num_aliens);
+    game.player.x = 112 - 5;
+    game.player.y = 32;
+    game.player.life = 3;
+
+    for (size_t yi = 0; yi < 5; ++yi)
+    {
+        for (size_t xi = 0; xi < 11; ++xi)
+        {
+            game.aliens[yi * 11 + xi].x = 16 * xi + 20;
+            game.aliens[yi * 11 + xi].y = 17 * yi + 128;
+        }
+    }
+
     // Game loop
     auto clear_color = rgb_to_uint32(0, 128, 0);
+    int player_move_dir = 1;
 
     while(!glfwWindowShouldClose(window))
     {
         buffer_clear(&buffer, clear_color);
 
-        buffer_sprite_draw(&buffer, alien_sprite, 112, 127,
-            rgb_to_uint32(128, 0, 0));
+        for (size_t ai = 0; ai < game.num_aliens; ++ai)
+        {
+            const auto& alien = game.aliens[ai];
+            size_t current_frame = alien_animation->time / alien_animation->frame_duration;
+            const auto& sprite = alien_animation->frames[current_frame];
+            buffer_sprite_draw(&buffer, sprite, alien.x, alien.y,
+                rgb_to_uint32(128, 0, 0));
+        }
 
-        buffer_sprite_draw(&buffer, alien_sprite, 112, 128, 
+        buffer_sprite_draw(&buffer, player_sprite, game.player.x, game.player.y,
             rgb_to_uint32(128, 0, 0));
+        
+        ++alien_animation->time;
+
+        if (alien_animation->time == alien_animation->num_frames * alien_animation->frame_duration)
+        {
+            if (alien_animation->loop)
+            {
+                alien_animation->time = 0;
+            }
+            else
+            {
+                alien_animation.reset();
+            }
+        }
 
         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0,
             buffer.width, buffer.height,
             GL_RGBA, GL_UNSIGNED_INT_8_8_8_8,
-            buffer.data
+            buffer.data.data()
         );
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
         
         glfwSwapBuffers(window);
+
+        if (game.player.x + player_sprite.width + player_move_dir >= game.width - 1)
+        {
+            game.player.x = game.width - player_sprite.width - player_move_dir - 1;
+            player_move_dir *= -1;
+        }
+        else if ((int) game.player.x + player_move_dir <= 0)
+        {
+            game.player.x = 0;
+            player_move_dir *= -1;
+        }
+        else
+        {
+            game.player.x += player_move_dir;
+        }
+
         glfwPollEvents();
     }
 
@@ -269,8 +400,8 @@ void main(void) {
     glfwTerminate();
     glDeleteVertexArrays(1, &fullscreen_triangle_vao);
 
-    delete[] alien_sprite.data;
-    delete[] buffer.data;
+    // delete[] alien_sprite.data;
+    // delete[] buffer.data;
 
     return 0;
 }
